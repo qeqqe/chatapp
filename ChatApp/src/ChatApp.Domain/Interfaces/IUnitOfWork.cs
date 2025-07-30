f6603d218752caeb2951d057ae8b2758f2d30a15
qeqqe <armansingh3172006@gmail.com>
2025-07-30 23:19:45 +0530

    Feat(Models): Add Chat, Message and User DTO
    Add WebSocket messages dto

---
+++ ChatApp/src/ChatApp.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+using ChatApp.Domain.Entities;
+
+namespace ChatApp.Domain.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<User> Users { get; }
+    IRepository<Chat> Chats { get; }
+    IRepository<Message> Messages { get; }
+    IRepository<ChatParticipant> ChatParticipants { get; }
+    IRepository<MessageRead> MessageRead { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+
+
+}
